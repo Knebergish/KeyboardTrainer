71d1b45d2b8460cd0e2c1c7a55d54354bc717d08
Temon137 <ak973@yandex.ru>
2018-11-24 17:58:44 +0400

    Ещё codestyle.

--- src/KeyboardTrainer/Main.java
+++ src/KeyboardTrainer/Main.java
@@ -12,24 +12,24 @@ import java.io.IOException;
 
 
 public class Main extends Application {
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		FXMLLoader loader     = new FXMLLoader(getClass().getResource("forms/MainForm.fxml"));
 		Parent     root       = loader.load();
 		MainForm   controller = loader.getController();
-
+		
 		primaryStage.setTitle("Keeeeeey!");
 		primaryStage.setWidth(700);
 		primaryStage.setHeight(400);
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
-
-
+		
+		
 		primaryStage.show();
 		controller.init();
 	}
