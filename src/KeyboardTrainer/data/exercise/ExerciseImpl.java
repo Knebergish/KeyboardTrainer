a03d8b1d371c5ec11618d8bcd4f9a49f0f59aedb
Temon137 <ak973@yandex.ru>
2018-12-14 14:07:20 +0400

    Изменена заглушная реализация Exercise для вывода тестовых данных.

--- src/KeyboardTrainer/data/exercise/ExerciseImpl.java
+++ src/KeyboardTrainer/data/exercise/ExerciseImpl.java
@@ -3,53 +3,80 @@ package KeyboardTrainer.data.exercise;
 
 import KeyboardTrainer.data.KeyboardZone;
 
+import java.util.HashSet;
 import java.util.Set;
 
 
+// Заглушка
 public class ExerciseImpl implements Exercise {
+	private final String            name;
+	private final int               level;
+	private final int               length;
+	private final String            text;
+	private final Set<KeyboardZone> keyboardZones;
+	private final int               maxErrorsCount;
+	private final long              maxAveragePressingTime;
+	private final int               id;
+	
+	public ExerciseImpl(String name, int level, int length, String text,
+	                    Set<KeyboardZone> keyboardZones, int maxErrorsCount, long maxAveragePressingTime, int id) {
+		this.name = name;
+		this.level = level;
+		this.length = length;
+		this.text = text;
+		this.maxErrorsCount = maxErrorsCount;
+		this.maxAveragePressingTime = maxAveragePressingTime;
+		this.id = id;
+		
+		this.keyboardZones = new HashSet<>();
+		for (int i = 0; i < level + 1; i++) {
+			this.keyboardZones.add(KeyboardZone.valueOf("ZONE_" + (i + 1)));
+		}
+	}
+	
 	@Override
 	public String getName() {
-		return null;
+		return name;
 	}
 	
 	@Override
 	public int getLevel() {
-		return -13;
+		return level;
 	}
 	
 	@Override
 	public int getLength() {
-		return 0;
+		return length;
 	}
 	
 	@Override
 	public String getText() {
-//		return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-		StringBuilder s = new StringBuilder();
-		for (int i = 0; i < 10; i++) {
-			s.append(i).append("\n");
-		}
-		s.append(0);
-		return s.toString();
+		return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+//		StringBuilder s = new StringBuilder();
+//		for (int i = 0; i < 10; i++) {
+//			s.append(i).append("\n");
+//		}
+//		s.append(0);
+//		return s.toString();
 	}
 	
 	@Override
 	public Set<KeyboardZone> getKeyboardZones() {
-		return null;
+		return keyboardZones;
 	}
 	
 	@Override
 	public int getMaxErrorsCount() {
-		return 0;
+		return maxErrorsCount;
 	}
 	
 	@Override
 	public long getMaxAveragePressingTime() {
-		return 0;
+		return maxAveragePressingTime;
 	}
 	
 	@Override
 	public int getId() {
-		return 0;
+		return id;
 	}
 }
