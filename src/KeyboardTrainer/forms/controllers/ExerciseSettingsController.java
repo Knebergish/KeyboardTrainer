708a52f29749440d0e5dfde9fd0e31db40ae7bcd
Temon137 <ak973@yandex.ru>
2018-12-25 20:48:09 +0400

    На форме изменения настроект упражнения: Удалена пятая клавиатурная зона; Реализована генерация текста заданной длины; Реализована загрузка текста из файла (только UTF-8!!!). Проверки и ограничения ждите в следующих сериях.

--- src/KeyboardTrainer/forms/controllers/ExerciseSettingsController.java
+++ src/KeyboardTrainer/forms/controllers/ExerciseSettingsController.java
@@ -4,16 +4,20 @@ package KeyboardTrainer.forms.controllers;
 import KeyboardTrainer.data.KeyboardZone;
 import KeyboardTrainer.data.exercise.Exercise;
 import KeyboardTrainer.data.exercise.ExerciseImpl;
+import KeyboardTrainer.forms.general.AlertFormManager;
 import KeyboardTrainer.language.Language;
 import KeyboardTrainer.language.RussianLanguage;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
+import KeyboardTrainer.randomize.SimpleTextRandomizer;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Window;
 import javafx.stage.WindowEvent;
 
-import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -36,28 +40,18 @@ public class ExerciseSettingsController {
 	public void init(Exercise exercise) {
 		newExercise = null;
 		
-		for (int i = 1; i < 6; i++) {
+		for (int i = 1; i < 5; i++) {
 			levelChoiceBox.getItems().add(i);
 		}
-		levelChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-			updateLevel(newValue);
-		});
 		
 		languageChoiceBox.getItems().add(new RussianLanguage());
 		
 		saveButton.setOnAction(event -> {
-			Set<KeyboardZone> zones = zonesGridPane.getChildren().stream()
-			                                       .filter(node -> !node.isDisabled())
-			                                       .map(node -> (CheckBox) node)
-			                                       .filter(CheckBox::isSelected)
-			                                       .map(CheckBox::getText)
-			                                       .map(Integer::valueOf)
-			                                       .map(KeyboardZone::byNumber)
-			                                       .collect(Collectors.toSet());
+			Set<KeyboardZone> zones = getKeyboardZones();
 			
 			newExercise = new ExerciseImpl(titleTextField.getText(),
 			                               levelChoiceBox.getValue(),
-			                               Integer.valueOf(lengthTextField.getText()),
+			                               textTextArea.getText().length(),
 			                               textTextArea.getText(),
 			                               zones,
 			                               Integer.valueOf(maxErrorsCountTextField.getText()),
@@ -77,28 +71,102 @@ public class ExerciseSettingsController {
 		lengthTextField.setText(String.valueOf(exercise.getLength()));
 		maxErrorsCountTextField.setText(String.valueOf(exercise.getMaxErrorsCount()));
 		maxAveragePressingTimeTextField.setText(String.valueOf(exercise.getMaxAveragePressingTime()));
-		levelChoiceBox.getSelectionModel().select(exercise.getLevel());
+		levelChoiceBox.getSelectionModel().select(exercise.getLevel() - 1);
+		selectKeyboardZones(exercise.getKeyboardZones());
+		languageChoiceBox.getSelectionModel().select(new RussianLanguage()); //TODO: добавить язык в упражнение
+		textTextArea.setText(exercise.getText());
+		
+		generateTextButton.setOnAction(event -> generateText());
+		selectFileButton.setOnAction(event -> loadTextFromFile());
+	}
+	
+	private void selectKeyboardZones(Set<KeyboardZone> zones) {
 		zonesGridPane.getChildren().stream()
 		             .filter(node -> !node.isDisabled())
 		             .map(node -> (CheckBox) node)
-		             .filter(checkBox -> exercise.getKeyboardZones()
-		                                         .contains(KeyboardZone.byNumber(Integer.valueOf(checkBox.getText()))))
+		             .filter(checkBox -> zones.contains(KeyboardZone.byNumber(Integer.valueOf(checkBox.getText()))))
 		             .forEach(checkBox -> checkBox.setSelected(true));
-		languageChoiceBox.getSelectionModel().select(new RussianLanguage()); //TODO: доавить язык в упражнение
-		textTextArea.setText(exercise.getText());
 	}
 	
-	private void updateLevel(Integer level) {
-		ObservableList<Node> children = zonesGridPane.getChildren();
-		for (int i = 0; i < children.size(); i++) {
-			boolean  disable  = i >= level;
-			CheckBox checkBox = (CheckBox) children.get(i);
-			
-			checkBox.setDisable(disable);
-			if (disable) {
-				checkBox.setSelected(false);
-			}
+	private void generateText() {
+		SimpleTextRandomizer simpleTextRandomizer = new SimpleTextRandomizer(languageChoiceBox.getValue(),
+		                                                                     getKeyboardZones());
+		textTextArea.setText(simpleTextRandomizer.generateText(Integer.parseInt(lengthTextField.getText())));
+	}
+	
+	private void loadTextFromFile() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Выбор файла с текстом");
+		FileChooser.ExtensionFilter extFilter =
+				new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+		fileChooser.getExtensionFilters().add(extFilter);
+		
+		//TODO: нормальное получение window
+		File file = fileChooser.showOpenDialog(titleTextField.getScene().getWindow());
+		if (file == null) {
+			return;
 		}
+		
+		List<String> strings;
+		try {
+			strings = Files.readAllLines(file.toPath());
+		} catch (IOException e) {
+			AlertFormManager.showAlert(Alert.AlertType.ERROR,
+			                           "Ошибка открытия файла",
+			                           "Невозможно считать данные из выбранного файла.",
+			                           "Проверьте наличие файла и корректность его расширения.",
+			                           null);
+			e.printStackTrace();
+			return;
+		}
+		String text = String.join("\n", strings);
+		
+		Language language = languageChoiceBox.getSelectionModel().getSelectedItem();
+		
+		// Формируем соответствие символов их зоне
+		HashMap<List<Character>, KeyboardZone> charsInZone =
+				Arrays.stream(KeyboardZone.values())
+				      .collect(Collectors.toMap(language::getSymbols,
+				                                o -> o,
+				                                (characters, characters2) -> null,
+				                                HashMap::new));
+		
+		// Формируем список всех допустимых символов выбранного языка
+		List<Character> allCharacters = charsInZone.keySet().parallelStream()
+		                                           .flatMap(Collection::parallelStream).collect(Collectors.toList());
+		allCharacters.add(' ');
+		allCharacters.add('\n');
+		
+		// Оставляем в тексте только допустимые символы
+		String trueText = text.chars().parallel()
+		                      .mapToObj(c -> (char) c)
+		                      .filter(c -> allCharacters.contains(Character.toLowerCase(c)))
+		                      .map(String::valueOf)
+		                      .collect(Collectors.joining());
+		
+		// Выбираем зоны, которые используются в тексте
+		Set<KeyboardZone> zones = trueText.chars().parallel()
+		                                  .mapToObj(c -> (char) c)
+		                                  .map(c -> charsInZone.get(charsInZone.keySet()
+		                                                                       .parallelStream()
+		                                                                       .filter(chars -> chars.contains(c))
+		                                                                       .findFirst()
+		                                                                       .orElse(null)))
+		                                  .collect(Collectors.toSet());
+		selectKeyboardZones(zones);
+		
+		textTextArea.setText(trueText);
+	}
+	
+	private Set<KeyboardZone> getKeyboardZones() {
+		return zonesGridPane.getChildren().stream()
+		                    .filter(node -> !node.isDisabled())
+		                    .map(node -> (CheckBox) node)
+		                    .filter(CheckBox::isSelected)
+		                    .map(CheckBox::getText)
+		                    .map(Integer::valueOf)
+		                    .map(KeyboardZone::byNumber)
+		                    .collect(Collectors.toSet());
 	}
 	
 	public Exercise getNewExercise() {
