7993accc7597a6fef3d0bcc15fa8a683aaa8c851
Temon137 <ak973@yandex.ru>
2019-01-08 12:23:18 +0400

    Добавлена форма общей статистики обучаемого.

---
+++ src/KeyboardTrainer/forms/controllers/statistics/AverageStatisticsController.java
@@ -0,0 +1,30 @@
+package KeyboardTrainer.forms.controllers.statistics;
+
+
+import KeyboardTrainer.forms.general.ContentArea;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+
+public class AverageStatisticsController implements ContentArea {
+	private final AverageStatistics statistics;
+	
+	@FXML
+	private Label averageErrorsCountLabel;
+	@FXML
+	private Label averagePressingTimeLabel;
+	@FXML
+	private Label averageTotalTimeLabel;
+	
+	
+	public AverageStatisticsController(AverageStatistics statistics) {
+		this.statistics = statistics;
+	}
+	
+	@Override
+	public void init() {
+		averageErrorsCountLabel.setText(String.format("%.2f", statistics.getAverageErrorsCount()));
+		averagePressingTimeLabel.setText(String.format("%.2f мс", statistics.getAveragePressingTime()));
+		averageTotalTimeLabel.setText(String.format("%.2f мс", statistics.getAverageTotalTime()));
+	}
+}
--- src/KeyboardTrainer/forms/general/PupilForm.java
+++ src/KeyboardTrainer/forms/general/PupilForm.java
@@ -1,15 +1,17 @@
 package KeyboardTrainer.forms.general;
 
 
+import KeyboardTrainer.Session;
 import KeyboardTrainer.data.exercise.ExerciseImpl;
-import KeyboardTrainer.forms.controllers.exercise.player.ExercisePlayerController;
-import KeyboardTrainer.forms.controllers.exercise.tree.ExerciseChooserController;
-import KeyboardTrainer.forms.general.menu.ChangeContentMenuButton;
-import KeyboardTrainer.forms.general.menu.CustomActionMenuButton;
+import KeyboardTrainer.data.statistics.StatisticsDAO;
 import KeyboardTrainer.forms.common.fxml.FXMLManager;
 import KeyboardTrainer.forms.common.fxml.RootWithController;
+import KeyboardTrainer.forms.controllers.exercise.player.ExercisePlayerController;
+import KeyboardTrainer.forms.controllers.exercise.tree.ExerciseChooserController;
+import KeyboardTrainer.forms.controllers.statistics.AverageStatisticsController;
+import KeyboardTrainer.forms.general.menu.ChangeContentMenuButton;
+import KeyboardTrainer.forms.general.menu.CustomActionMenuButton;
 import KeyboardTrainer.language.Language;
-import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 
@@ -17,39 +19,38 @@ public class PupilForm extends GeneralForm {
 	public PupilForm() {
 		setTitle("Лучший клавиатурный тренажёр");
 		addMenuButton(
-				new CustomActionMenuButton("Продолжить",
-				                           () -> {
-					                           RootWithController<ExercisePlayerController> rootWithController =
-							                           FXMLManager.load("ExercisePlayer");
-					
-					                           Stage stage = FXMLManager.createStage(
-							                           rootWithController.getRoot(),
-							                           "Ваше следующее упражнение");
-					
-					                           rootWithController.getController().init(
-							                           //TODO: искать первое непройденное
-							                           new ExerciseImpl("TestExercise",
-							                                            1,
-							                                            "12345",
-							                                            null,
-							                                            12,
-							                                            1000,
-							                                            -1,
-							                                            Language.RUSSIAN));
-					                           stage.setResizable(false);
-					                           stage.show();
-				                           }));
+				new CustomActionMenuButton("Продолжить", this::continueAction));
 		addMenuButton(new ChangeContentMenuButton("Выбор упражнения", "ExerciseGeneral",
 		                                          new ExerciseChooserController()));
-		addMenuButton(new CustomActionMenuButton("Статистика",
-		                                         () -> {
-			                                         Alert alert = new Alert(Alert.AlertType.WARNING);
-			                                         alert.setTitle("Внимание");
-			                                         alert.setHeaderText("Ведутся технические работы");
-			                                         alert.setContentText(
-					                                         "Данная форма пока недоступна, вернитесь позже.");
-			                                         alert.show();
-		                                         }));
+		addMenuButton(new ChangeContentMenuButton("Статистика", "AverageStatistics",
+		                                          getAverageStatisticsController()));
 		addMenuButton(new ChangeContentMenuButton("Справка", "About"));
+	}
+	
+	private void continueAction() {
+		RootWithController<ExercisePlayerController> load = FXMLManager.load("ExercisePlayer");
+		
+		Stage stage = FXMLManager.createStage(load.getRoot(), "Ваше следующее упражнение");
+		stage.setResizable(false);
+		
+		load.getController().init(
+				//TODO: искать первое непройденное
+				new ExerciseImpl("TestExercise",
+				                 1,
+				                 "12345",
+				                 null,
+				                 12,
+				                 1000,
+				                 -1,
+				                 Language.RUSSIAN));
+		
+		
+		stage.show();
+	}
+	
+	private AverageStatisticsController getAverageStatisticsController() {
+		int userId     = Session.getLoggedUser().getId();
+		var statistics = StatisticsDAO.getInstance().getAverageStatisticsForUser(userId);
+		return new AverageStatisticsController(statistics);
 	}
 }
