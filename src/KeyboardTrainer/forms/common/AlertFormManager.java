10c42317559d3e5fc1474f6d6c10d187693fd01b
Temon137 <ak973@yandex.ru>
2019-01-11 13:50:49 +0400

    Окно Alert теперь имеет более минималистичный вид, без иконки.

--- src/KeyboardTrainer/forms/common/AlertFormManager.java
+++ src/KeyboardTrainer/forms/common/AlertFormManager.java
@@ -4,8 +4,7 @@ package KeyboardTrainer.forms.common;
 import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.DialogEvent;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class AlertFormManager {
@@ -13,9 +12,9 @@ public class AlertFormManager {
 	                             EventHandler<DialogEvent> closeEvent) {
 		Alert alert = new Alert(alertType);
 		alert.setTitle(title);
-		((Stage) alert.getDialogPane().getScene().getWindow()).getIcons().add(new Image("file:res/icon.jpg"));
 		alert.setHeaderText(headerText);
 		alert.setContentText(text);
+		alert.initStyle(StageStyle.UTILITY);
 		
 		if (closeEvent != null) {
 			alert.setOnCloseRequest(closeEvent);
