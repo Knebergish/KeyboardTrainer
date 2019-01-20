de29f98ceab1f90ea56a0da74cf45f8cf2ae7b1f
Temon137 <ak973@yandex.ru>
2019-01-20 13:09:32 +0400

    Теперь в логине при регистрации можно использовать только буквы и цифры. После регистрации открывается форма авторизации, а не приложения.

--- src/KeyboardTrainer/forms/controllers/login/RegistrationController.java
+++ src/KeyboardTrainer/forms/controllers/login/RegistrationController.java
@@ -4,10 +4,12 @@ package KeyboardTrainer.forms.controllers.login;
 import KeyboardTrainer.data.user.User;
 import KeyboardTrainer.data.user.UserDAO;
 import KeyboardTrainer.data.user.UserImpl;
+import KeyboardTrainer.forms.common.AlertFormManager;
 import KeyboardTrainer.forms.common.Validator;
 import KeyboardTrainer.forms.common.Validator.Checker;
 import KeyboardTrainer.forms.controllers.ActionHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -28,23 +30,27 @@ public class RegistrationController implements LoginController {
 	@FXML
 	private Button        cancelButton;
 	
-	private Consumer<User> loginUser;
+	private ActionHandler cancelHandler;
 	private Validator      validator;
 	
 	@Override
 	public void init(Consumer<User> loginUser, ActionHandler cancelHandler) {
-		this.loginUser = loginUser;
+		this.cancelHandler = cancelHandler;
 		
 		registerButton.setOnAction(event -> register());
 		cancelButton.setOnAction(event -> cancelHandler.handle());
 		
 		validator = new Validator(
-				List.of(new Checker(() -> UserDAO.getInstance().getByLogin(loginTextField.getText()) == null,
-				                    "Пользователь с таким именем уже существует.",
-				                    "Введите другое имя пользователя."),
+				List.of(new Checker(() -> loginTextField.getText().chars().mapToObj(value -> (char) value)
+				                                        .allMatch(Character::isLetterOrDigit),
+				                    "Логин может содержать только буквы и цифры.",
+				                    "Удалите из логина недопустимые символы."),
 				        new Checker(() -> loginTextField.getText().length() >= 3,
 				                    "Слишком короткий логин.",
 				                    "Длина логина должна быть не менее 3-х символов."),
+				        new Checker(() -> UserDAO.getInstance().getByLogin(loginTextField.getText()) == null,
+				                    "Пользователь с таким именем уже существует.",
+				                    "Введите другое имя пользователя."),
 				        new Checker(() -> primoPasswordField.getText().length() >= 5,
 				                    "Слишком короткий пароль.",
 				                    "Длина пароля должна быть не менее 5-и символов."),
@@ -59,8 +65,13 @@ public class RegistrationController implements LoginController {
 		if (validator.validateOrAlert()) {
 			User newUser = new UserImpl(-1, loginTextField.getText(), primoPasswordField.getText(),
 			                            false, false);
-			newUser = UserDAO.getInstance().create(newUser);
-			loginUser.accept(newUser);
+			UserDAO.getInstance().create(newUser);
+			cancelHandler.handle();
+			AlertFormManager.showAlert(Alert.AlertType.INFORMATION,
+			                           "Успешная регистрация",
+			                           "Регистрация успешно завершена.",
+			                           "Теперь вы можете войти в программу с вашими данными.",
+			                           null);
 		}
 	}
 }
